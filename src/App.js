4386fdf2d9d6dc4703c2adafae7289f9caaf503a
micolCODES <micolromano124@gmail.com>
2022-11-28 12:06:41 +0100

    4.3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to my APP!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents.js';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CitySearch />
+        <NumberOfEvents />
+        <EventList />
+      </div>
+    );
+  }
 }
 
 export default App;
