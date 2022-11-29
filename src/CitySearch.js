43b8c0dc93b116bd4bc016bedda28ef94f289c21
micolCODES <micolromano124@gmail.com>
2022-11-29 10:43:24 +0100

    4.7

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -32,6 +32,7 @@ class CitySearch extends Component {
     this.setState({
       query: suggestion,
       showSuggestions: false,
+      infoText: '',
     });
     this.props.updateEvents(suggestion);
   };
